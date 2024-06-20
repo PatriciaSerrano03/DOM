68cbd6641d192f94489805471c10291f56c0da8c
PatriiSerrano <patricia.serranomolina@gmail.com>
2024-06-20 11:10:35 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const titleElemnt = document.getElementById('title');
+
+console.dir(titleElemnt);
+console.log(titleElemnt.clientWidth);
+
+console.dir(titleElemnt.parentElement);
+console.dir(titleElemnt.nextElementSibling);
